d0f7045ddce4f14f24a4a2e084d4b376501a6efa
MohamedTenzali <m.tenzali@gmail.com>
2026-05-27 19:45:27 +0200

    Sla wijzigingen op

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "arabicokids-v66";
+const CACHE_VERSION = "arabicokids-v67";
 const APP_ROOT = self.registration.scope;
 const INDEX_URL = new URL("index.html", APP_ROOT).href;
 const CORE_CACHE = [
