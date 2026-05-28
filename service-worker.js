f27168bf96e0f29521c20391dfab297d272f7653
MohamedTenzali <m.tenzali@gmail.com>
2026-05-28 09:36:29 +0200

    Auto: commit workspace changes via Copilot

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "arabicokids-v71";
+const CACHE_VERSION = "arabicokids-v73";
 const APP_ROOT = self.registration.scope;
 const INDEX_URL = new URL("index.html", APP_ROOT).href;
 const CORE_CACHE = [
