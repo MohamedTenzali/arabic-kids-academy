084a35730036a38bba6ca64d66637c4730d31032
MohamedTenzali <m.tenzali@gmail.com>
2026-05-30 15:24:46 +0200

    Update content and assets - latest changes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "arabicokids-v2026-launch-02";
+const CACHE_NAME = "arabicokids-v2026-footer-mobile-01";
 const APP_ROOT = self.registration.scope;
 const INDEX_URL = new URL("index.html", APP_ROOT).href;
 const CORE_CACHE = [
