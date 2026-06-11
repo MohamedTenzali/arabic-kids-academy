e31fdcc359b7ce7bc5c4b7af89ffce637b5a7f45
MohamedTenzali <m.tenzali@gmail.com>
2026-06-11 18:59:05 +0200

    ✅ Constrain support section width and enlarge mug illustration

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "arabicokids-v2026-49";
+const CACHE_NAME = "arabicokids-v2026-50";
 const APP_ROOT = self.registration.scope;
 const INDEX_URL = new URL("index.html", APP_ROOT).href;
 const CORE_CACHE = [
